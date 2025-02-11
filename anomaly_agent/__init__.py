36901d4542d3aab66361db602b5890be91514943
Andrew Maguire <andrewm4894@gmail.com>
2025-02-11 20:30:13 +0000

    Bump version to 0.2.0

--- anomaly_agent/__init__.py
+++ anomaly_agent/__init__.py
@@ -2,7 +2,7 @@ from .agent import AnomalyAgent, Anomaly, AnomalyList
 from .plot import plot_df
 from .utils import make_df, make_anomaly_config
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 __all__ = [
     "AnomalyAgent",
--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ except FileNotFoundError:
 
 setup(
     name="anomaly-agent",
-    version="0.1.0",
+    version="0.2.0",
     author="Andrew Maguire",
     author_email="andrewm4894@gmail.com",
     description="A package for detecting anomalies in time series data using LLMs",
