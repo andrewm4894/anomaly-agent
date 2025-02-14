bbcb795ce031dad54c274ca15c3484fdbdb97436
Andrew Maguire <andrewm4894@gmail.com>
2025-02-14 21:34:44 +0000

    Bump version to 0.5.0

--- anomaly_agent/__init__.py
+++ anomaly_agent/__init__.py
@@ -2,7 +2,7 @@ from .agent import AnomalyAgent, Anomaly, AnomalyList
 from .plot import plot_df
 from .utils import make_df, make_anomaly_config
 
-__version__ = "0.4.0"
+__version__ = "0.5.0"
 
 __all__ = [
     "AnomalyAgent",
