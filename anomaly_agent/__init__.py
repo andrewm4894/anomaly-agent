4b81ac296df08cd533eb80dc0c05cfb5761c5614
Andrew Maguire <andrewm4894@gmail.com>
2026-01-30 23:07:16 +0000

    Bump version to 0.10.0
    - Sync __init__.py version with pyproject.toml
    - Published to PyPI with PostHog LLM analytics integration

--- anomaly_agent/__init__.py
+++ anomaly_agent/__init__.py
@@ -8,7 +8,7 @@ from .agent import Anomaly, AnomalyAgent, AnomalyList
 from .plot import plot_df
 from .utils import make_anomaly_config, make_df
 
-__version__ = "0.8.0"
+__version__ = "0.10.0"
 
 __all__ = [
     "AnomalyAgent",
